b447f3ba7fac1092000af3e5e920e21bfb75dec9
Rubics14 <102100272+Rubics14@users.noreply.github.com>
2022-04-10 17:55:37 -0500

    changes

---
+++ web-mobile-programs/html-css-js-basics/html-css-js-basics 2/script.js
@@ -0,0 +1,15 @@
+let counter = 0;
+
+//camelCase
+function buttonClicked()
+{
+  counter++;
+
+  let clickedCounterElement = document.getElementById("clickcounter");
+
+  clickedCounterElement.innerHTML = "Clicked" + counter + " times.";
+
+let clickedButtonElement = document.getElementById("clickbutton");
+
+clickedButtonElement.addEventListener("click", buttonClicked());
+}
